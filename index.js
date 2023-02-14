61ad36af25f18192bbe5b027fa84f7fcade6038e
Shilpashreeau <shilpashreehebbar@gmail.com>
2023-02-14 12:23:16 -0500

    Questions from A-D solved

--- index.js
+++ index.js
@@ -11,11 +11,92 @@
 
 // I. Variables & Data Types
 // A. Q + A
+/*How do we assign a value to a variable?
+A: Using an assignment operator
+
+How do we change the value of a variable?
+A: first we have to assign the value to a variable using "var " or "let" keywords then we can change value of that variable just assigning the variable name to new value.
+Example:
+let a=10;
+a=20;
+
+
+How do we assign an existing variable to a new variable?
+A: First we have to create a variable and assign to a value using var or let keywords then assign first variable to second using assignment operator.
+ Example: let a=10;
+    let b=a;
+
+
+Remind me, what are declare, assign, and define?
+A: Creating a variable in JavaScript is called "declaring" a variable. 
+Storing a value to a declared variable using assignment operator is called "assign".
+Creating a name for block of code is called "define"
+
+What is pseudocoding and why should you do it?
+A: Easy to understand text description that has only logic but not actual code written in any language.
+
+What percentage of time should be spent thinking about how you're going to solve a problem vs actually typing in code to solve it?
+A:  I would take 80% to think and 20% to type.
+
 // B. Strings
+Create a variable called firstVariable
+Assign it the value of the string "Hello World"
+Change the value of this variable to some number
+Store the value of firstVariablein a new variable called secondVariable
+Change the value of secondVariableto any string.
+What is the value of firstVariable?
+Create a variable called yourName and set it equal to your name as a string. Then, write an expression that takes the string "Hello, my name is " and the variable yourName so that it returns a new string with them concatenated.
+ex: Hello, my name is Jean Valjean
+A:
+let firstVariable="Hello World";
+firstVariable=100;
+let secondVariable=firstVariable;
+secondVariable="Welcome";
+console.log(firstVariable);
+let yourName="Shilpa";
+let expression="Hello, my name is ";
+console.log(expression+yourName);
+
+
 // C. Booleans
+  const a = 4;
+  const b = 53;
+  const c = 57;
+  const d = 16;
+  const e = 'Kevin';
+
+  console.log(a < b);
+  console.log(c > d);
+  console.log('Name' === 'Name');
+  // FOR THE NEXT TWO, USE ONLY && OR ||
+  console.log(true || false);
+  console.log(false || false || false || false || false || true);
+  console.log(false && false && false && false && false || true);
+  console.log(false == false)
+  console.log(e === 'Kevin');
+  console.log(a + b == c); // note: a < b < c is NOT CORRECT (and is not a valid JS expression, think about using other math operations)
+  console.log(a * a == d); // note: the answer is a simple arithmetic equation, not something "weird"
+  console.log(48 == '48');
+ 
+
 // D. The Farm
+Declare a variable animal. Set it to be either "cow" or something else
+Write code that will print out "mooooo" if the it is equal to cow
+Change your code so that if the variable animal is anything other than a cow, it will print "Hey! You're not a cow."
+Commit
+let animal="cat";
+if(animal==="cow"){
+console.log("moooo");
+}else{
+    console.log("Hey! You're not a cow.");
+}
+
+
+
 // E. Driver's Ed
-//_____________________________________________________________
+//Make a variable that holds a person's age; be semantic
+Write code that will print out "Here are the keys!", if the age is 16 years or older, or, if the age is younger than 16, a message should print "Sorry, you're too young."
+
 
 
 
