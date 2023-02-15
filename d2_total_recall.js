716a261734523edd3ccfe38395b310b5e8900021
Shilpashreeau <shilpashreehebbar@gmail.com>
2023-02-15 12:23:00 -0500

    Wild Wild Life answered

--- d2_total_recall.js
+++ d2_total_recall.js
@@ -34,9 +34,30 @@ console.log("Buzz" );
 
 ////////////////////////////////
 //* Wild Wild Life
-////////////////////////////////
 
+const wolfy = ["Wolfy", "wolf", 16, "Yukon Territory"]
+const sharky = ["Sharky", "shark", 20, "Left Coast"]
+const plantee = ["Plantee", "plant",  5000 , "Mordor"]
+const porgee = ["Porgee", "Porg", 186, "Ahch-To"]
+const dart = ["D'Art" , "Demogorgan Dog", 2, "Upside Down"]
 
+//Plantee just had her birthday; change Plantee's array to reflect her being a year older.
+plantee[2]+=1;
+console.log(plantee);
+
+//Change Wolfy's hometown from "Yukon Territory" to "Gotham City".
+wolfy[3]="Gotham City";
+console.log(wolfy);
+
+//Give D'Art a second hometown by adding "Hawkins"
+dart.push("Hawkins");
+console.log(dart);
+
+//Porgee decides that Wolfy can't be named "Wolfy" anymore. Remove "Wolfy" from the wolfyarray and replace it with "Gameboy".
+wolfy.shift();
+console.log(wolfy);
+wolfy.unshift("Gameboy");
+console.log(wolfy);
 ////////////////////////////////
 //* Yell at the Ninja Turtles
 ////////////////////////////////
