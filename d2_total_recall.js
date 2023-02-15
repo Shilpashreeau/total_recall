17a7ce77b73f89e2ac003f2a9a224b8f8a6ea3e2
Shilpashreeau <shilpashreehebbar@gmail.com>
2023-02-15 12:12:46 -0500

    Fizz Buzz answered

--- d2_total_recall.js
+++ d2_total_recall.js
@@ -1,7 +1,7 @@
 ////////////////////////////////
 // *Easy Going
 //Write a for loop that will log the numbers 1 through 20.
-for(i=1;i<=20;i++){
+for(let i=1;i<=20;i++){
 console.log(i);
 }
 
@@ -9,14 +9,28 @@ console.log(i);
 ////////////////////////////////
 //* Get Even
 //Write a for loop that will log only the even numbers in 0 through 200.
-for(i=0;i<=200;i+=2){
+for(let i=0;i<=200;i+=2){
 console.log(i);
 }
 
 ////////////////////////////////
 //* Fizz Buzz
-////////////////////////////////
+/*Write a javascript application that logs all numbers from 1 - 100.
+If a number is divisible by 3 log "Fizz" instead of the number.
+If a number is divisible by 5 log "Buzz" instead of the number.
+If a number is divisible by 3 and 5 log "FizzBuzz" instead of the number.*/
 
+for(let num=1;num<=100;num++){
+if(num%3===0 && num%5===0){
+    console.log("FizzBuzz");
+
+}else if(num%3===0){
+console.log("Fizz");
+}else if(num%5===0){
+console.log("Buzz" );
+}
+
+}
 
 ////////////////////////////////
 //* Wild Wild Life
